891d2b3b7811984596b013cee24d9522a42216aa
Mario Petek <mariopetek0@gmail.com>
2024-05-26 14:26:52 +0200

    Remove body parser

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import { itemRouter } from './routes/itemRoutes'
 import { restaurantRouter } from './routes/restaurantRoutes'
 import { errorHandler } from './middleware/errorHandler'
 import { orderRouter } from './routes/orderRoutes'
-import bodyParser from 'body-parser'
 import { tableRouter } from './routes/tableRoutes'
 
 dotenv.config()
@@ -17,8 +16,6 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-
-app.use(bodyParser.json())
 
 app.use(express.json())
 
